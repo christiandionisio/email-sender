6d29ae2a901676c146c8647f8787f7dcb44c4990
Christian Dionisio <christiandionisio9432@gmail.com>
2021-04-24 13:10:53 -0500

    Envio de mail con datos estaticos

--- src/main/java/com/cdionisio/emailsender/controller/EmailSenderController.java
+++ src/main/java/com/cdionisio/emailsender/controller/EmailSenderController.java
@@ -1,5 +1,66 @@
 package com.cdionisio.emailsender.controller;
 
-public class EmailSenderController {
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSenderController {
+	
+	@PostMapping
+	public String sendEmail() throws AddressException, MessagingException, IOException {
+		sendmail();
+		return "Email enviado";
+	}
+	
+	
+	private void sendmail() throws AddressException, MessagingException, IOException {
+		   Properties props = new Properties();
+		   props.put("mail.smtp.auth", "true");
+		   props.put("mail.smtp.starttls.enable", "true");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
+		   props.put("mail.smtp.port", "587");
+		   
+		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+		      protected PasswordAuthentication getPasswordAuthentication() {
+		         return new PasswordAuthentication("xxxxx@gmail.com", "xxxxx");
+		      }
+		   });
+		   Message msg = new MimeMessage(session);
+		   msg.setFrom(new InternetAddress("xxxxxxx@gmail.com", false));
+
+		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("xxxxx@gmail.com"));
+		   msg.setSubject("Tutorials point email");
+		   msg.setContent("Tutorials point email", "text/html");
+		   msg.setSentDate(new Date());
+
+		   MimeBodyPart messageBodyPart = new MimeBodyPart();
+		   messageBodyPart.setContent("Tutorials point email", "text/html");
+
+		   Multipart multipart = new MimeMultipart();
+		   multipart.addBodyPart(messageBodyPart);
+		   MimeBodyPart attachPart = new MimeBodyPart();
+
+		   attachPart.attachFile("C:\\Users\\USER\\Downloads\\icono.png");
+		   multipart.addBodyPart(attachPart);
+		   msg.setContent(multipart);
+		   Transport.send(msg);   
+		}
 }
